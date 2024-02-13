6e23510729ddc3b340c1db87373fb117f716a5a2
motionbit95 <motionbit.dev@gmail.com>
2024-02-13 17:10:14 +0900

    ✨Feat : first test component

---
+++ dist/Test.js
@@ -0,0 +1,6 @@
+// src/lib/Test
+import React from "react";
+const Test = () => {
+  return /*#__PURE__*/React.createElement("div", null, "\uBC30\uD3EC\uC6A9 \uCEF4\uD3EC\uB10C\uD2B8");
+};
+export default Test;
---
+++ src/lib/Test.jsx
@@ -0,0 +1,8 @@
+// src/lib/Test
+import React from "react";
+
+const Test = () => {
+  return <div>배포용 컴포넌트</div>;
+};
+
+export default Test;
